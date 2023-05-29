9a702d9cf60076ecafb055d0f768cc79d1262a08
Omer <omerpolak2166@gmail.com>
2023-05-29 19:07:59 +0300

    make the triangle isosceles and can be moved by the arrow keys

--- Triangle game.asm
+++ Triangle game.asm
@@ -3,71 +3,82 @@
 
 .DATA
     ; The emulator is slow so you need to put low numbers in it to work acceptably fast.
-    height DW 20     ; Triangle height
-    width  DW 20     ; Triangle width
+    height DW 12     ; Triangle height
+    width  DW 12     ; Triangle width
+    horizontal_move dw 90h
+    vertical_move dw 65h
+    rowLength dw ?
+    space dw ?
+    
 .CODE
+START:
+Main proc 
+    PUSH BP
+    MOV BP, SP
+    
     MOV AX, @DATA   ; Initialize data segment
     MOV DS, AX                               
     PUSH height
     PUSH width
-    MOV AH, 0
-    MOV AL, 13h
-    INT 10H
+    CALL WindowSettings
     CALL DrawTriangle
+    
+
     input: ;everything from this part is still not working
         CALL MoveTriangle
         PUSH height
         PUSH width
+        xor si,si
         call DrawTriangle
     jmp input
-        
-    MOV AH, 4Ch     ; Exit program
-    INT 21h
-
+    
+    POP BP
+    RET
+endp Main
 
 DrawTriangle proc
     PUSH BP
     MOV BP, SP
     
-    mov ah, 09h
-    mov cx, 1000h
-    mov al, 20h
-    mov bl, 17  ; This is Blue & White.
-    INT 10H
+    call ClearScreen
+    call calcRow
     
-    MOV AH, 0CH
-    MOV CX, 50H
-    MOV DX, 50H
-    MOV AL, 20H     ; Set pixel color
+    MOV CX, horizontal_move 
+    MOV DX, vertical_move
+
     loop1:
-        push bx
-        mov bx, [bp+4]
+    
+    MOV AL, 0Fh     ; Set pixel color      
+    MOV AH, 0CH
+        
+        mov bx, [bp+4] ; width
         loop2:
             INT 10H
             inc cx
             dec bx
             cmp bx, 0
             jne loop2
-        pop bx
-        cmp bx, 0
-        je doneDrawing
-        add si, 1
-        mov cx, 50h
-        add cx, si
-        dec dx
-        dec [bp+4]
-        dec [bp+4]
-        cmp [bp+4], 0
+        
+        mov bx, [bp+4]
+        sub bx, rowLength
+        mov [bp+4], bx
+
+        dec dx  
+        dec [bp+6]
+        
+        call calcSpace
+        mov cx, horizontal_move
+        add cx, space
+        
+        cmp [bp+6], 0
         je doneDrawing
         jmp loop1
-    doneDrawing:          
-    POP BP
-    RET
+    doneDrawing:
+    pop bp          
+    RET 4 ; clear stack
 endp DrawTriangle
 
 MoveTriangle proc 
-    PUSH BP
-    MOV BP, SP
     waitForInput:
         mov ah, 0h
         int 16h
@@ -77,17 +88,92 @@ MoveTriangle proc
     
         ; Check if the input is an arrow key (AH = 48, 50, 52, or 54)
         cmp ah, 48h ; Up arrow
-        je valid
+        je up    
         cmp ah, 50h ; Down arrow
-        je valid
+        je down
         cmp ah, 4dh ; Left arrow
-        je valid
+        je left
         cmp ah, 4bh ; Right arrow
-        je valid
+        je right
         jmp waitForInput  
-    valid:
-        POP BP
-        RET
+    up:
+        sub vertical_move , 30
+        add horizontal_move, 0
+        jmp move  
+    down:
+        add vertical_move , 30
+        add horizontal_move, 0
+        jmp move 
+    left:
+        add vertical_move , 0
+        add horizontal_move, 30
+        jmp move 
+    right:
+        add vertical_move , 0
+        sub horizontal_move, 30
+        jmp move 
+    move:
+    ret     
 endp MoveTriangle
-END
 
+WindowSettings proc 
+    MOV AH, 0
+    MOV AL, 13h ;interupt 10h mode 0, 13
+    INT 10H
+    RET
+endp WindowSettings
+
+ClearScreen proc   
+    mov ah, 09h
+    mov cx, 1000h
+    mov al, 20h
+    mov bl, 40h ; This is Blue & White.
+    INT 10H
+    ret
+endp ClearScreen
+
+calcRow proc   
+    push bp
+    mov bp, sp
+    
+    push dx
+    mov ax, width ; width
+    mov bx, height ; height
+    dec bx
+    cwd  ; prevent overflow 
+    div bx
+    pop dx
+    mov rowLength, ax 
+    
+    pop bp
+    ret
+endp calcRow
+
+calcSpace proc   
+    
+    push bp
+    mov bp, sp
+    
+    push dx
+    mov ax, width
+    mov bx, [bp+10] ; current height
+    
+    sub ax, bx
+    mov bx, 2
+    cmp ax, 0
+    je ifZero
+    cwd ; prevent overflow
+    div bx
+    jmp next 
+    ifZero:
+        mov ax, 0
+    next:    
+    mov space, ax
+    pop dx
+    pop bp
+    ret
+endp calcSpace
+
+; we calc the points  for each row - take the height -1 and divide the width with it and this should be the number of pixels to decrease for each row - the first row will be always the width.
+; (width - 1) / height 
+END START                       
